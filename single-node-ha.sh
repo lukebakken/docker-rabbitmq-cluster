6879659954e56bad57fa59996231dae7a356bebe
Luke Bakken <luke@bakken.io>
2025-05-15 11:43:29 -0700

    use local rabbitmqadmin

--- single-node-ha.sh
+++ single-node-ha.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+script_dir="$(CDPATH='' cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
+readonly script_dir
+
 set -o errexit
 set -o nounset
 set -o pipefail
 
 declare -r queue="${1:-ha-queue-0-0}"
-rabbitmqadmin declare operator_policy "name=ha-single-$queue" "pattern=^$queue$" 'apply-to=queues' 'definition={"ha-mode":"exactly","ha-params":1,"ha-sync-mode":"automatic"}' priority=99
+
+"$script_dir/rabbitmqadmin" declare operator_policy "name=ha-single-$queue" "pattern=^$queue$" 'apply-to=queues' 'definition={"ha-mode":"exactly","ha-params":1,"ha-sync-mode":"automatic"}' priority=99
